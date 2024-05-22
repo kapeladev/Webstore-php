f822137174f72f4756a016d1df70529f4cdc9607
kapeladev <mkapeladev@gmail.com>
2024-05-22 11:40:21 +0200

    done

--- shopdb.sql
+++ shopdb.sql
@@ -30,31 +30,9 @@ CREATE TABLE `subcategory` (
   `category_id` int
 );
 
-CREATE TABLE `orderDetails` (
-  `id` integer PRIMARY KEY,
-  `order_id` int,
-  `product_id` int,
-  `quantity` int,
-  `price` decimal
-);
-
-CREATE TABLE `order` (
-  `id` integer PRIMARY KEY,
-  `user_id` int,
-  `totalamount` decimal,
-  `status` varchar(10),
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
-
 ALTER TABLE `products` ADD FOREIGN KEY (`markid`) REFERENCES `mark` (`id`);
 
 ALTER TABLE `subcategory` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
 ALTER TABLE `products` ADD FOREIGN KEY (`subcategory_id`) REFERENCES `subcategory` (`id`);
 
-ALTER TABLE `order` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `orderDetails` ADD FOREIGN KEY (`order_id`) REFERENCES `order` (`id`);
-
-ALTER TABLE `orderDetails` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
